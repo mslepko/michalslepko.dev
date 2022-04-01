ae94e1ac6e363bfe210d138ee99c3fe7acb2de63
Michal Slepko <michal.slepko@gmail.com>
2022-04-01 09:58:23 +0100

    updating hero to display lines conditionally when they are defined

--- src/components/hero.js
+++ src/components/hero.js
@@ -11,9 +11,8 @@ const Hero = () => (
   >
     <HeroLine lineNo="1" text="// Hello World!" />
     <HeroLine lineNo="2" varName="$name" varValue="'Michal';" />
-    <HeroLine lineNo="3" varName="$description" varValue="A web developer based in the UK';" />
+    <HeroLine lineNo="3" varName="$description" varValue="'A web developer based in the UK';" />
   </div>
 )
 
 export default Hero
-
--- src/components/heroline.js
+++ src/components/heroline.js
@@ -1,30 +1,24 @@
 import * as React from "react"
 
-const HeroLine = ({lineNo, text, varName, varValue}) => (
-  <p
-  className="text-2xl"
->
-  <span
-    className="text-dark-blue pr-4"
-  >
-    {lineNo}.
-  </span>
-  <span
-    className="text-stone-500"
-  >
-      {text}
-  </span>
-  <span
-    className="text-purple"
-  >
-    {varName}&nbsp;=&nbsp;
-  </span>
-  <span
-    className="text-blue"
-  >
-    {varValue}
-  </span>
-</p>
-)
+const HeroLine = ({lineNo, text, varName, varValue}) => {
+  const textOutput = text ? <span className="text-stone-500">{text}</span> : null;
+  const nameOutput = varName ? <span className="text-purple">{varName}&nbsp;=&nbsp;</span> : null;
+  const valueOutput = varValue ? <span className="text-blue">{varValue}</span> : null;
+
+  return (
+    <p
+      className="text-2xl"
+    >
+    <span
+      className="text-dark-blue pr-4"
+    >
+      {lineNo}.
+    </span>
+    {textOutput}
+    {nameOutput}
+    {valueOutput}
+  </p>
+  )
+}
 
 export default HeroLine
