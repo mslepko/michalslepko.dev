8e70d7129d3b66607d11340d0b7946c9456439d0
Michal Slepko <michal.slepko@gmail.com>
2022-04-29 11:56:03 +0100

    Added GitHub and LinkedIn social links
    Moved icons to a separate folder

--- src/components/socials.js
+++ src/components/socials.js
@@ -1,32 +1,51 @@
 import * as React from "react"
 
-import Twitter from "../images/twitter-icon.svg"
-import Instagram from "../images/instagram-icon.svg"
-import Twitch from "../images/twitch-icon.svg"
+import Twitter from "../images/icons/twitter-icon.svg"
+import Instagram from "../images/icons/instagram-icon.svg"
+import Twitch from "../images/icons/twitch-icon.svg"
+import Github from "../images/icons/github-icon.svg"
+import LinkedIn from "../images/icons/linkedin.svg"
 
 // Icons from = https://simpleicons.org/
 
 
 const Socials = () => (
   <div
-    className="flex justify-around flex-nowrap w-3/5 m-auto"
+    className="flex justify-around flex-nowrap w-4/5 m-auto"
   >
       <a href="https://twitter.com/michal_codes"
         target="_blank"
+        class="pr-2"
         rel="noopener noreferrer">
-          <img src={Twitter} className="w-8 h-8" />
+          <img src={Twitter} className="w-8 h-8" alt="Twitter"/>
       </a>
 
       <a href="https://instagram.com/michal.codes"
         target="_blank"
+        class="pr-2"
         rel="noopener noreferrer">
-          <img src={Instagram} className="w-8 h-8" />
+          <img src={Instagram} className="w-8 h-8" alt="Instagram"/>
       </a>
 
       <a href="https://twitch.com/michal_codes"
         target="_blank"
+        class="pr-2"
         rel="noopener noreferrer">
-          <img src={Twitch} className="w-8 h-8" />
+          <img src={Twitch} className="w-8 h-8" alt="Twitch"/>
+      </a>
+
+      <a href="https://github.com/mslepko"
+        target="_blank"
+        class="pr-2"
+        rel="noopener noreferrer">
+          <img src={Github} className="w-8 h-8" alt="GitHub"/>
+      </a>
+
+      <a href="https://www.linkedin.com/in/michalslepko"
+        target="_blank"
+        class="pr-2"
+        rel="noopener noreferrer">
+          <img src={LinkedIn} className="w-8 h-8" alt="LinkedIn"/>
       </a>
   </div>
 )
