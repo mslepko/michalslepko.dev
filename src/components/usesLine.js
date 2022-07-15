3ea6c44d65cb42da97e8e164e1974fadb354bc30
Michal Slepko <michal.slepko@gmail.com>
2022-07-15 19:08:49 +0100

    Removed unused variable

--- src/components/usesLine.js
+++ src/components/usesLine.js
@@ -22,7 +22,6 @@ const UsesLine = ({type}) => {
         }
 
         for (let url of urls) {
-          console.log('url', url)
           link.url = setupItem[url]
           link.name += ` (${url})`
 
@@ -36,7 +35,7 @@ const UsesLine = ({type}) => {
   }
 
   return (
-    <div className="{type} md:w-2/3 m-auto marker:text-sky-400 list-disc md:pl-5 space-y-6 text-slate-500 mt-8">
+    <div className="md:w-2/3 m-auto marker:text-sky-400 list-disc md:pl-5 space-y-6 text-slate-500 mt-8">
       {links}
     </div>
   )
