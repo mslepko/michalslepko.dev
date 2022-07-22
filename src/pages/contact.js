97fabcb6e79df7c5b5f44877585843beadf7d359
Michal Slepko <michal.slepko@gmail.com>
2022-07-22 10:06:24 +0100

    small fixes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,5 @@
 //import * as React from "react"
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
