c74f6470637c724aadb40ed7d0616f1b81502e54
Michal Slepko <michal.slepko@gmail.com>
2022-10-16 22:56:13 +0100

    stats updates

--- src/lists/socialStats.js
+++ src/lists/socialStats.js
@@ -1,8 +1,8 @@
 const socialStats = {
-  'Instagram': '71.9k',
+  'Instagram': '73.2k',
   'TikTok': '4.4k',
-  'Twitch': '155',
-  'Twitter': '113',
+  'Twitch': '156',
+  'Twitter': '119',
 }
 
 export default socialStats
