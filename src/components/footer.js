33755960dbcfac9ed4ec290e424fbf6a447244b7
Michal Slepko <michal.slepko@gmail.com>
2023-01-26 00:11:09 +0000

    Added telemetry analytics

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,13 @@
 import * as React from "react"
+import { TelemetryDeck } from '@telemetrydeck/sdk';
+const td = new TelemetryDeck({ app: '85F1CA88-24D2-4798-9F55-4C1573C42928', user: 'anonymous' });
 
 const Footer = () => {
+  td.signal()
+  const url = typeof window !== 'undefined' ? window.location.href : '';
+  td.signal({
+    route: url,
+  });
   return (
     <footer
       className="container
