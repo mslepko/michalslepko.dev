3004c65b2bf0ff626c842a28c857fdc80a9db054
Michal Slepko <michal@rootscope.co.uk>
2023-02-11 12:20:05 +0000

    Added canonical-urls

--- gatsby-config.js
+++ gatsby-config.js
@@ -115,6 +115,12 @@ module.exports = {
     },
   },
   },
+  {
+    resolve: `gatsby-plugin-canonical-urls`,
+    options: {
+      siteUrl: `https://michalslepko.dev/`,
+    },
+  },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
