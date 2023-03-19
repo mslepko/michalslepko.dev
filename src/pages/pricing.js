9a4658e2182eec163f3fc09a841695f3a50f1775
Michal Slepko <michal@rootscope.co.uk>
2023-03-19 23:16:20 +0000

    light/dark mode on pricing page

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -8,35 +8,35 @@ import UsesLine from "../components/usesLine"
 const UsesPage = () => (
   <Layout>
     <div className="container mx-auto p-8 pb-16 text-center max-w-screen-xl">
-      <h1 className="text-2xl sm:text-3xl font-bold text-mint tracking-tight dark:text-slate-200 pt-10 text-center pb-16">
+      <h1 className="text-2xl sm:text-3xl font-bold text-darkest dark:text-lightest tracking-tight pt-10 text-center pb-16">
         Pricing
       </h1>
       <div className="pt-14">
-        <p className="text-mint pb-1 text-xl">
+        <p className="text-darkest dark:text-lightest pb-1 text-xl">
           If you found this page it means you want to collaborate with me!
         </p>
-        <p className="text-mint pb-1 text-xl">
+        <p className="text-darkest dark:text-lightest pb-1 text-xl">
           This page contains information about me, links to my previous
           partnerships, my stats and my pricing.
         </p>
-        <p className="text-mint pb-1 text-xl">
+        <p className="text-darkest dark:text-lightest pb-1 text-xl">
           If you are interested or have questions please email me at{" "}
-          <a href="mailto:michal@rootscope.co.uk" className="text-light-blue">
+          <a href="mailto:michal@rootscope.co.uk" className="text-brand hover:text-darkest dark:text-brand dark:hover:text-white">
             michal@rootscope.co.uk
           </a>
         </p>
       </div>
 
-      <h3 className="text-center text-xl sm:text-1xl font-bold text-mint tracking-tight dark:text-slate-200 pt-14">
+      <h3 className="text-center text-xl sm:text-1xl font-bold text-darkest dark:text-lightest tracking-tight  pt-14">
         About me
       </h3>
-      <p className="text-mint text-left pb-1 text-xl">
+      <p className="text-darkest dark:text-lightest text-left pb-1 text-xl">
         My name is Michal Slepko, I'm a software engineer based in the UK. I'm
         creating content related to my day-to-day job as a developer. <br />I
         post mainly about software development, productivity, working from home
         and having fun while doing what you love.
       </p>
-      <p className="text-mint pb-1 text-xl text-left">
+      <p className="text-darkest dark:text-lightest pb-1 text-xl text-left">
         I create content on Instagram and then repurpose it to TikTok and
         YouTube. I'm also streaming on Twitch where you can find me coding or
         just co-working with others.
@@ -44,16 +44,16 @@ const UsesPage = () => (
 
       <div className="flex flex-col md:flex-row gap-10">
         <div className="flex-1">
-          <h3 className="text-center text-xl sm:text-1xl font-bold text-mint tracking-tight dark:text-slate-200 pt-14">
+          <h3 className="text-center text-xl sm:text-1xl font-bold text-darkest dark:text-lightest tracking-tight  pt-14">
             Social Media links
           </h3>
           <UsesLine type="socials" addClasses="w-full md:w-full" />
         </div>
         <div>
-          <h3 className="text-center text-xl sm:text-1xl font-bold text-mint tracking-tight dark:text-slate-200 pt-14">
+          <h3 className="text-center text-xl sm:text-1xl font-bold text-darkest dark:text-lightest tracking-tight  pt-14">
             Pricing
           </h3>
-          <ul className="text-dark bg-white rounded-lg shadow-md dark:text-gray-400 md:flex-row max-w-xl m-auto mt-8">
+          <ul className="pricing text-darkest bg-white rounded-lg shadow-md md:flex-row max-w-xl m-auto mt-8">
             <li className="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 transition-all duration-500 hover:bg-mint">
               <p className="font-bold">Link in bio - 99 USD/week</p>
               <p className="text-left">
@@ -107,10 +107,10 @@ const UsesPage = () => (
               </p>
             </li>
           </ul>
-          <p className="text-base text-mint">*All Reels are posted on TikTok and YT Shorts.</p>
+          <p className="text-base text-darkest dark:text-lightest">*All Reels are posted on TikTok and YT Shorts.</p>
         </div>
       </div>
-      <h3 className="text-center text-xl sm:text-1xl font-bold text-mint tracking-tight dark:text-slate-200 pt-14">
+      <h3 className="text-center text-xl sm:text-1xl font-bold text-darkest dark:text-lightest tracking-tight  pt-14">
         Social Media stats
       </h3>
       <StaticImage
@@ -124,7 +124,7 @@ const UsesPage = () => (
         className="block mx-auto"
       />
 
-      <p className="text-mint pb-1 text-xl mt-8">
+      <p className="text-darkest dark:text-lightest pb-1 text-xl mt-8">
         If you are interested or have questions please email me at{" "}
         <a href="mailto:michal@rootscope.co.uk" className="text-light-blue">
           michal@rootscope.co.uk
