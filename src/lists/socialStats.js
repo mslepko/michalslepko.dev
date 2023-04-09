18c71a2a4eedbef0e718fe74fa555b14277a81ef
Michal Slepko <michal@rootscope.co.uk>
2023-04-09 22:49:38 +0100

    stats update

--- src/lists/socialStats.js
+++ src/lists/socialStats.js
@@ -2,8 +2,8 @@ const socialStats = {
   Instagram: "83.7k",
   TikTok: "6.7k",
   Twitch: "155",
-  Twitter: "205",
-  YouTube: "149",
+  Twitter: "212",
+  YouTube: "154",
 }
 
 export default socialStats
