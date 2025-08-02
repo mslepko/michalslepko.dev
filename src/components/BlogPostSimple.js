c3c74c857d3eafb50a2ea97bbbbe8e705b276c29
Michal Slepko <michal@rootscope.co.uk>
2025-08-02 13:41:28 +0100

    new snippets

--- src/components/BlogPostSimple.js
+++ src/components/BlogPostSimple.js
@@ -5,7 +5,7 @@ const BlogPostSimple = ({ children }) => {
   console.log("children", children)
   return (
     <Layout>
-      <div className="container mx-auto p-8 pb-16 text-left text-mint">
+      <div className="container mx-auto p-8 pb-16 text-left text-mint my-snippets">
         <div>{children}</div>
       </div>
     </Layout>
